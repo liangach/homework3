7017ab8fe75c4ea75a89ff67f1336f30cf903f50
liangach <liangach3942@outlook.com>
2024-10-22 22:27:03 +0800

    Create BOOK.cs

---
+++ BOOK/BOOK.cs
@@ -0,0 +1,12 @@
+namespace BOOK3.Models
+{
+    public class Book3
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Publisher { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
